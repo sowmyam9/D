1e8075960244e3eb1e0cc2fdacc54b142ac5d206
sowmyam9 <challaeswaraiah14379@gmail.com>
2025-02-20 12:57:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.metrics import accuracy_score
 
 st.set_page_config(page_title="Diabetes Prediction", layout="wide", page_icon="🧑‍⚕️")
 
-d_model_path=r"C:\Users\ChallaEswaraiah\OneDrive\Desktop\P\d_model.sav"
+d_model_path=r"d_model.sav"
 d_model=pickle.load(open(d_model_path,'rb'))
 
 st.title("DP using ML")
@@ -65,7 +65,7 @@ if st.button('Diabetes Test Result'):
 
 if st.button('Show Model Accuracy'):
         
-        diabetes_dataset = pd.read_csv(r"C:\Users\ChallaEswaraiah\OneDrive\Desktop\P\diabetes.csv")
+        diabetes_dataset = pd.read_csv(r"diabetes.csv")
 
         X_test = diabetes_dataset.drop(columns=["Outcome"])
         y_test = diabetes_dataset["Outcome"]
